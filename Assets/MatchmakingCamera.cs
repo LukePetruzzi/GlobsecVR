5add6ce925339ebaa8ff12b95106d4f1a6a4f507
LukePetruzzi <lukepetruzzi@gmail.com>
2017-04-08 16:37:13 -0400

    added matchmaking camera

---
+++ Assets/MatchmakingCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchmakingCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // get rid of this camera when a game is joined
+		if (GameObject.Find("Player(Clone)") != null)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
