63a7adfe52ee598c9bb6d21b5ec5150590f3fba2
LukePetruzzi <lukepetruzzi@gmail.com>
2017-04-08 18:11:37 -0400

    testing simple local player thing with rotation of player

---
+++ Assets/MyNetworkedPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkedPlayerController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer)
+        {
+            return;
+        }
+        else
+        {
+            this.transform.rotation = Quaternion.Euler(new Vector3(12, 12, 12));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
