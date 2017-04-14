2f1914722dee30b008bcff7e6e310b7cc5e8fe06
LukePetruzzi <lukepetruzzi@gmail.com>
2017-04-13 21:53:01 -0400

    should have networked head, controllers now

--- Assets/_Scripts/MyNetworkedPlayerController.cs
+++ Assets/_Scripts/MyNetworkedPlayerController.cs
@@ -5,14 +5,25 @@ using UnityEngine.Networking;
 
 public class MyNetworkedPlayerController : NetworkBehaviour {
 
+    //source gameobjects head, left and right controller object of htc vive prefab
+
+    public GameObject rightContSource;
+    public GameObject leftContSource;
+    public GameObject headObjSource;
+
+
+    //prefabs to assign head, left controller, and right controller
+    public GameObject vrHeadObjPrefab;
+    public GameObject vrLeftCtrlPrefab;
+    public GameObject vrRightCtrlPrefab;
+
+    GameObject vrHeadObj;
+    GameObject vrLeftCtrl;
+    GameObject vrRightCtrl;
+
+
     public override void OnStartLocalPlayer()
     {
-        //Renderer[] rens = GetComponentsInChildren<Renderer>();
-        //foreach (Renderer ren in rens)
-        //{
-        //    ren.enabled = false;
-        //}
-
         GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
     }
 
@@ -41,6 +52,12 @@ public class MyNetworkedPlayerController : NetworkBehaviour {
             getChildGameObject(this.gameObject, "Eyewear").GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
             getChildGameObject(this.gameObject, "Hats").GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
 
+
+            // Instantiate head and controllers
+            CmdInstantiteHeadAndController();
+
+
+
             // change name so it's obvious whos local in the inspector
             gameObject.name = gameObject.name.Replace("(Clone)", "") + "_localPlayer";
 
@@ -53,20 +70,83 @@ public class MyNetworkedPlayerController : NetworkBehaviour {
             return;
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    //Instantiate on start head and vr controller object so that it can be view by normal players
+    //[Command]
+    void CmdInstantiteHeadAndController()
+    {
+        // instantiate the objects using the prefabs created
+        vrHeadObj = (GameObject)Instantiate(vrHeadObjPrefab);
+        vrHeadObj.gameObject.transform.parent = this.gameObject.transform;
+        vrHeadObj.name = vrHeadObjPrefab.name;
+
+        vrLeftCtrl = (GameObject)Instantiate(vrLeftCtrlPrefab);
+        vrLeftCtrl.gameObject.transform.parent = this.gameObject.transform;
+        vrLeftCtrl.name = vrLeftCtrlPrefab.name;
+
+        vrRightCtrl = (GameObject)Instantiate(vrRightCtrlPrefab);
+        vrRightCtrl.gameObject.transform.parent = this.gameObject.transform;
+        vrRightCtrl.name = vrRightCtrlPrefab.name;
+
+        // spawn the objects for the clients
+        NetworkServer.Spawn(vrHeadObj);
+        NetworkServer.Spawn(vrLeftCtrl);
+        NetworkServer.Spawn(vrRightCtrl);
+    }
+
+    // Update is called once per frame
+    void Update () {
         
-        
-	}
+        // only want to be updating on local players. This handles commands work as they should
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        // find the local hardware of the head and controllers if they haven't been found yet
+        if (headObjSource == null || leftContSource == null || rightContSource == null)
+        {
+ 
+            this.headObjSource = GameObject.Find("Camera (eye)");
+
+            // the controllers are actually finding the controller points on the hands I want for IK
+            this.leftContSource = GameObject.Find("leftHandTarget");
+            this.rightContSource = GameObject.Find("rightHandTarget");
+        }
+
+        //sync pos on network
+        CmdControllerPositionSync();
+    }
+
+
+    //sync position on VR controller objects so that VR player movemnts/action can be viewd by normal user
+    [Command]
+    public void CmdControllerPositionSync()
+    {
+        // head transform update
+        if (headObjSource != null)
+        {
+            vrHeadObj.transform.position = headObjSource.transform.position;
+            vrHeadObj.transform.rotation = headObjSource.transform.rotation;
+        }
+        // left controller transform update
+        if (leftContSource != null)
+        {
+            vrLeftCtrl.transform.position = leftContSource.transform.position;
+            vrLeftCtrl.transform.rotation = leftContSource.transform.rotation;
+        }
+        // right controller transform update
+        if (rightContSource != null)
+        {
+            vrRightCtrl.transform.position = rightContSource.transform.position;
+            vrRightCtrl.transform.rotation = rightContSource.transform.rotation;
+        }
+    }
 
     static public GameObject getChildGameObject(GameObject fromGameObject, string withName)
     {
-        //Author: Isaac Dart, June-13.
         Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
         foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
         return null;
     }
 }
-
-
