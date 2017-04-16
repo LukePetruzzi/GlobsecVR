28e21e30a047bf3e93dd3e9e52df34e2fd3a771d
LukePetruzzi <lukepetruzzi@gmail.com>
2017-04-16 14:29:31 -0400

    got canvas rotation to work

--- Assets/_Scripts/CanvasFacePlayer.cs
+++ Assets/_Scripts/CanvasFacePlayer.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class CanvasFacePlayer : MonoBehaviour {
 
+    public GameObject localPlayer;
+
 	// Use this for initialization
 	void Start () {
-		
+        localPlayer = GameObject.Find("Camera (eye)");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        Vector3 camRotation = Camera.main.transform.rotation.eulerAngles;
+
+        //this.transform.rotation.eulerAngles = Vector3(camRotation.x, 90, 90);
+        this.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation.y + 90, 90, 90));
+    }
 }
