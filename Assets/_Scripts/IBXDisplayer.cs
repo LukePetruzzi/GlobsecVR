bfd1d14765c26e1d4046629d4385faf24591efff
LukePetruzzi <lukepetruzzi@gmail.com>
2017-04-26 20:59:13 -0400

    now renderer will really vanish

---
+++ Assets/_Scripts/IBXDisplayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IBXDisplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.Find("IBX") != null)
+        {
+            // turn off the meshes
+            foreach (Renderer rend in GameObject.Find("IBX").GetComponentsInChildren<Renderer>())
+            {
+                rend.enabled = true;
+            }
+            foreach (Collider coll in GameObject.Find("IBX").GetComponentsInChildren<Collider>())
+            {
+                coll.enabled = true;
+            }
+        }
+    }
+
+}
---
+++ Assets/_Scripts/IBXVanisher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IBXVanisher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.Find("IBX") != null)
+        {
+            // turn off the meshes
+            foreach (Renderer rend in GameObject.Find("IBX").GetComponentsInChildren<Renderer>())
+            {
+                rend.enabled = false;
+            }
+            foreach (Collider coll in GameObject.Find("IBX").GetComponentsInChildren<Collider>())
+            {
+                coll.enabled = false;
+            }
+        }
+    }
+}
